741eb76bdab1bae41fb2475938ebab2e2d7a067b
Soumi Sarkar <90122541+SoumiOnGit@users.noreply.github.com>
2024-01-08 03:54:03 +0000

    ..

--- app.py
+++ app.py
@@ -1,29 +1,3 @@
-"""from flask import Flask, render_template, redirect, url_for, request
-from pysrt import open as open_srt
-import os
-app = Flask(__name__)
-
-app.config['UPLOAD_FOLDER'] = './static/music'
-
-@app.route('/')
-def index():
-    music_files = [file for file in os.listdir(app.config['UPLOAD_FOLDER']) if file.endswith('.mp3')]
-    return render_template('index.html', music_files=music_files, lyrics=lyrics)
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        music_file = request.files['music']
-        if music_file and music_file.filename.endswith('.mp3'):
-            filename = os.path.join(app.config['UPLOAD_FOLDER'], music_file.filename)
-            music_file.save(filename)
-            return redirect(url_for('index'))
-    return render_template('upload.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-"""
-
 from flask import Flask, render_template, redirect, url_for, request
 import os
 
@@ -58,7 +32,7 @@ lyrics_data = srt_to_lyrics(srt_file_path)
 def index():
     music_files = [file for file in os.listdir(app.config['UPLOAD_FOLDER']) if file.endswith('.mp3')]
     
-    return render_template('index1.html', music_files=music_files,lyrics=lyrics_data)
+    return render_template('index.html', music_files=music_files,lyrics=lyrics_data)
 
 
 @app.route('/upload', methods=['GET', 'POST'])
